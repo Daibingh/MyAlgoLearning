264ca411b1caff68d76719fd8855d95016fa0426
daibingh <daibingh@gmail.com>
2021-05-29 22:59:15 +0800

    change play

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,20 +1,62 @@
 
 // freopen("playground/data.in", "r", stdin);
 
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
+// #include <ctype.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <math.h>
+#include <bits/stdc++.h>
 
-#define MAX 200000
+using namespace std;
 
+void print_graph(const unordered_map<int, vector<int>>& graph) {
+    for (const auto& t : graph) {
+        cout << t.first << ": ";
+        for (const auto& k : t.second) {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+}
+
+void dfs(const unordered_map<int, vector<int>>& graph,
+         unordered_set<int>& visted,
+         int start) {
+    if (visted.count(start))
+        return;
+    visted.insert(start);
+    const auto t = graph.find(start);
+    if (t == graph.end())
+        return;
+    for (const auto& v : t->second) {
+        dfs(graph, visted, v);
+    }
+}
 
 int main() {
+    freopen("playground/data.in", "r", stdin);
 
-    int a[100] = {0};
+    unordered_map<int, vector<int>> graph;
 
-    for (int i=0;i<100;++i) {
-        printf("%d\n", a[i]);
+    string line;
+    while (getline(cin, line)) {
+        // cout<<line<<endl;
+        stringstream ss(line);
+        int u = 0, v = 0;
+        ss >> u;
+        while (ss >> v) {
+            graph[u].push_back(v);
+        }
+    }
+
+    // print_graph(graph);
+
+    unordered_set<int> visted;
+
+    dfs(graph, visted, 0);
+
+    for (const auto& u : visted) {
+        cout << u << endl;
     }
 }
